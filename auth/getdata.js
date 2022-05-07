f524f2139076ecfce92dfb4037e729d33e7cb900
flyingsonu122 <flyingsonu736@gmail.com>
2022-05-07 15:41:44 +0530

    up

---
+++ auth/getdata.js
@@ -0,0 +1,42 @@
+
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+
+    const tablebody = document.getElementById('tablebody');
+    var num = 0;
+    var outhtml = ``;
+
+    db.collection("admissionForm").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            num++;
+            // doc.data() is never undefined for query doc snapshots
+            const data = doc.data();
+            // console.log(doc.id, " => ", data);
+
+            outhtml += `<tr>
+                    <th scope="row">${num}</th>
+                    <td>${data.URN}</td>
+                    <td>${data.childName}</td>
+                    <td>${data.inputGender}</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+            
+            `;
+
+            tablebody.innerHTML = outhtml;
+
+
+
+
+
+
+
+
+
+
+
+        });
+    });
+
+});
