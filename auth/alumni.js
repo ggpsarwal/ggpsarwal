6acbfd4c77df02214aa0c522c6f80d2e4f796b00
flyingsonu122 <flyingsonu736@gmail.com>
2022-05-07 15:59:01 +0530

    up

---
+++ auth/alumni.js
@@ -0,0 +1,37 @@
+
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+
+    const tablebody = document.getElementById('tablebody');
+    var num = 0;
+    var outhtml = ``;
+
+    db.collection("alumni").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            num++;
+            // doc.data() is never undefined for query doc snapshots
+            const data = doc.data();
+            // console.log(doc.id, " => ", data);
+
+            outhtml += `<tr>
+                    <th scope="row">${num}</th>
+                    <td>${data.URN}</td>
+                    <td>${data.name}</td>
+                    <td>${data.Gender}</td>
+                    <td>${data.admissionDate}</td>
+                    <td>${data.passingDate}</td>
+                    
+                  </tr>
+            
+            `;
+
+            tablebody.innerHTML = outhtml;
+
+
+
+
+
+        });
+    });
+
+});
