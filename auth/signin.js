778785f1414dbce80f6c53225c40a09baa5bc9bc
flyingsonu122 <flyingsonu736@gmail.com>
2022-05-07 20:48:40 +0530

    change

--- auth/signin.js
+++ auth/signin.js
@@ -8,8 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
             var uid = user.uid;
             var tenantId = user.tenantId;
             const emailVerified = user.emailVerified
@@ -20,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // console.log(emailVerified);
             // console.log("creationTime : " + creationTime);
             // console.log("lastSignInTime : " + lastSignInTime);
+
             // getting data
             var docRef = db.collection("users").doc(`${uid}`);
 
@@ -67,12 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>`;
                     }
 
-                    outhtml += `<button class="btn btn-outline-success my-2 my-sm-0" onclick="resetPassword()" id="resetPassword">Reset Password</button>
-                    <br><br><button class="btn btn-outline-danger my-2 my-sm-0" onclick="deleteUser()" id="deleteAccount">Delete Account</button>
-                    <br><br>
-                    
-                    `;
-
                     document.querySelector('.userdata').innerHTML = outhtml;
 
                 } else {
@@ -97,13 +90,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Signed in
                 var user = userCredential.user;
                 // console.log(user);
-                // console.log(user.uid);
-                // console.log(user.email);
+                console.log(user.uid);
+                console.log(user.email);
 
                 alert("Sign In Successfull!");
 
                 inputForm.reset();
-
 
 
             })
@@ -129,35 +121,3 @@ function signOut() {
 }
 
 
-
-function resetPassword() {
-    const user = firebase.auth().currentUser;
-    firebase.auth().sendPasswordResetEmail(user.email)
-        .then(() => {
-            alert("Password reset email sent!");
-
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-
-        });
-}
-
-function deleteUser() {
-    const user = firebase.auth().currentUser;
-    alert(`${user.uid}`);
-    db.collection("users").doc(`${user.uid}`).delete().then(() => {
-        console.log("Document successfully deleted!");
-    }).catch((error) => {
-        console.log("Error removing document: ", error);
-    });
-
-    user.delete().then(() => {
-        alert("User deleted.");
-        
-    }).catch((error) => {
-        console.log(error);
-    });
-}
