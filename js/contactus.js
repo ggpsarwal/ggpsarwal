abd62baf18b21f7b723fd407097905ab569a7ad2
flyingsonu122 <flyingsonu736@gmail.com>
2022-05-08 08:24:39 +0530

    change

---
+++ js/contactus.js
@@ -0,0 +1,27 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+    // contact form
+    const contactForm = document.querySelector(".contact-form");
+    const fullName = document.getElementById("fullname");
+    const emailAddress = document.getElementById("emailaddress");
+    const subject = document.getElementById("subject");
+    const message = document.getElementById("message");
+
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        db.collection('contact-form').doc().set({
+            fullName: fullName.value,
+            emailAddress: emailAddress.value,
+            subject: subject.value,
+            message: message.value,
+        }).then(() => {
+            contactForm.reset();
+            alert("thank you for your message");
+        });
+    });
+    // contact form
+
+});
+
+
+
